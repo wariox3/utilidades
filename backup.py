3c2e3b8120eef1a131d7eeee16e0bbe55aca809a
MarioA <maestradaz3@gmail.com>
2025-07-18 09:31:39 -0500

    feat: script para leer acces.log

--- backup.py
+++ backup.py
@@ -1,40 +1,139 @@
 import subprocess
 import datetime
+import sys
+import psycopg2
+from psycopg2 import sql
 from decouple import config
+from pathlib import Path
+from urllib.parse import quote
 
-
-# Configuración de la conexión
-host = config('DATABASE_HOST')
-port = config('DATABASE_PORT')
-usuario = config('DATABASE_USER')
-clave = config('DATABASE_CLAVE')
-basedatos = "bdinsepltda"
+servidor = config('PG_DATABASE_HOST')
+puerto = config('PG_DATABASE_PORT')
+usuario = config('PG_DATABASE_USER')
+clave = quote(config('PG_DATABASE_CLAVE'))
+base_datos = config('PG_DATABASE_NAME')
 timestamp = datetime.datetime.now().strftime("%y%m%d%H%M")
-archivo_salida = f"/home/desarrollo/Escritorio/{basedatos}_{timestamp}.sql"
+archivo_salida = f"/home/desarrollo/Escritorio/{base_datos}.sql"
+base_datos_backup = "bditriobackup"
 
 def exportar_base_datos():    
-    print(port)
-    comando = [
-        "mysqldump",
-        f"--host={host}",
-        f"--port={port}",
-        f"--user={usuario}",
-        f"--password={clave}",
-        "--routines",
-        "--events",
-        "--single-transaction",
-        "--quick",
-        "--skip-lock-tables",
-        basedatos,
-        f"--ignore-table={basedatos}.gen_log"
-    ]
+    connection_string = f"postgresql://{usuario}:{clave}@{servidor}:{puerto}/{base_datos}"
     
+    comando = [
+        'pg_dump',
+        '-d', connection_string,
+        '-f', archivo_salida,
+        '-v',
+    ]
+
     try:
-        with open(archivo_salida, "w", encoding="utf-8") as f:
-            subprocess.run(comando, check=True, stdout=f)
+        subprocess.run(comando, check=True)
         print(f"✅ Dump generado correctamente: {archivo_salida}")
     except subprocess.CalledProcessError as e:
         print("❌ Error al generar el dump:", e)
 
+def restaurar_backup():
+    if not Path(archivo_salida).exists():
+        print("❌ El archivo de backup no existe.")
+        return False
+    connection_string = f"postgresql://{usuario}:{clave}@{servidor}:{puerto}/bditriobackup"
+    
+    try:
+        subprocess.run([
+            'psql',
+            '-d', connection_string,
+            '-f', archivo_salida,
+            '-v', 'ON_ERROR_STOP=1',  # Detiene la ejecución si hay un error SQL
+            '--echo-errors',          # Muestra errores en la consola
+        ], check=True)
+        
+        print(f"✅ Restauración exitosa en bditriobackup")
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"❌ Error al restaurar: {e}")
+        return False
+    
+def restaurar_backup_estandar():
+
+    if not Path(archivo_salida).exists():
+        print(f"❌ El archivo de backup {archivo_salida} no existe.")
+        return False
+    
+    try:                
+        subprocess.run([
+            'pg_restore',
+            '--verbose',
+            '--clean',
+            '--no-owner',
+            '--dbname', f"postgresql://{usuario}:{clave}@{servidor}:{puerto}/bditriobackup",
+            archivo_salida
+        ], check=True)
+        
+        print(f"✅ Restauración exitosa en bditriobackup")
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"❌ Error al restaurar: {e}")
+        return False    
+
+def modificar_dominio():
+    try:
+        connection_string = f"postgresql://{usuario}:{clave}@{servidor}:{puerto}/bditriobackup"
+        conn = psycopg2.connect(connection_string)
+        cursor = conn.cursor()
+        
+        select_query = sql.SQL("SELECT id, domain FROM public.cnt_dominio")
+        cursor.execute(select_query)
+        registros = cursor.fetchall()
+        
+        for registro in registros:
+            id_registro, dominio_actual = registro
+            if dominio_actual and '.' in dominio_actual:                                
+                nuevo_dominio = dominio_actual.split('.')[0] + ".localhost"
+                if id_registro == 1:                    
+                    nuevo_dominio = "localhost"                
+                update_query = sql.SQL("""
+                    UPDATE public.cnt_dominio 
+                    SET domain = %s 
+                    WHERE id = %s
+                """)
+                cursor.execute(update_query, (nuevo_dominio, id_registro))    
+        conn.commit()
+        print("Dominios modificados exitosamente")
+        
+    except Exception as e:
+        print(f"Error al modificar dominios: {e}")
+        if 'conn' in locals():
+            conn.rollback()
+    finally:
+        # Cerrar conexión
+        if 'cursor' in locals():
+            cursor.close()
+        if 'conn' in locals():
+            conn.close()
+
+def mostrar_menu():
+    print("\nSeleccione una opción:")
+    print("b - backup")
+    print("r - restaurar")
+    print("e - restaurar estandar")
+    print("m - modificar dominio")
+
+    print("s - Salir")    
+    opcion = input("Opción: ").lower().strip()    
+    if opcion == 'b':
+        exportar_base_datos()
+    elif opcion == 'r':
+        restaurar_backup()   
+    elif opcion == 'e':
+        restaurar_backup_estandar()    
+    elif opcion == 'm':
+        modificar_dominio()                           
+    elif opcion == 's':
+        print("Saliendo del programa...")
+        sys.exit(0)
+    else:
+        print("Opción no válida. Intente nuevamente.")
+        mostrar_menu()
+
 if __name__ == "__main__":
-    exportar_base_datos()
+    mostrar_menu()
---
+++ leer_log_apache.py
@@ -0,0 +1,169 @@
+from decouple import config
+import mysql.connector
+import psycopg2
+import sys
+import traceback
+import re
+
+def crear_conexiones():    
+    postgres_parametros = {
+        'user': config('PG_DATABASE_USER'),
+        'password': config('PG_DATABASE_CLAVE'),
+        'host': config('PG_DATABASE_HOST'),
+        'port': config('PG_DATABASE_PORT'),
+        'database': config('PG_DATABASE_NAME')
+    }
+    
+    try:
+        pg_conn = psycopg2.connect(**postgres_parametros)
+        pg_cursor = pg_conn.cursor()
+        
+        return pg_conn, pg_cursor
+        
+    except Exception as e:
+        print(f"Error al conectar a las bases de datos: {e}")
+        sys.exit(1)
+
+def cerrar_conexiones(pg_conn):
+    try:
+        if pg_conn and not pg_conn.closed:
+            pg_conn.close()
+        print("Conexiones cerradas.")
+    except Exception as e:
+        print(f"Error al cerrar conexiones: {e}")   
+
+def crear_tabla(pg_conn):
+    with pg_conn.cursor() as cursor:
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS access_logs (
+            id SERIAL PRIMARY KEY,
+            host TEXT,
+            ip TEXT,
+            remote_user TEXT,
+            auth_user TEXT,
+            timestamp TEXT,
+            method TEXT,
+            path TEXT,
+            protocol TEXT,
+            status INTEGER,
+            bytes INTEGER,
+            referer TEXT,
+            user_agent TEXT,
+            processed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        ''')
+        pg_conn.commit()
+
+def parse_log_line(line):
+    try:
+        # Patrón regex para parsear líneas del access.log
+        #pattern = r'^(\S+) (\S+) (\S+) (\S+) \[([^\]]+)\] "(\S+) (\S+) (\S+)" (\d+) (\d+) "([^"]*)" "([^"]*)"'
+        #match = re.match(pattern, line.strip())  # <-- Aquí se usa strip()
+        #pattern = r'^(\S+) (\S+) (\S+) ([^[]+) \[([^\]]+)\] "(\S+) (\S+) (\S+)" (\d{3}) (\d+|-) "([^"]*)" "([^"]*)"'
+        #match = re.match(pattern, line.strip())
+        #pattern = r'^(\S+) (\S+) (\S+) (\S+) \[([^\]]+)\] "(?:(\\S+) (\\S+) (\\S+)|([^"]*))" (\d{3}) (\d+|-) "([^"]*)" "([^"]*)"'
+        #match = re.match(pattern, line.strip())        
+        pattern = r'^(\S+) (\S+) (\S+) (\S+) \[([^\]]+)\] "(\S+) (\S+) (\S+)" (\d{3}) (\d+|-) "([^"]*)" "([^"]*)"'
+        match = re.match(pattern, line.strip())
+        if not match:
+            print(f"No se pudo parsear la línea: {line}")
+            return None
+        bytes_sent = 0 if match.group(10) == '-' else int(match.group(10))
+        return {
+            'host': match.group(1),
+            'ip': match.group(2),
+            'remote_user': match.group(3),
+            'auth_user': match.group(4).strip(),
+            'timestamp': match.group(5),
+            'method': match.group(6),
+            'path': match.group(7),
+            'protocol': match.group(8),
+            'status': int(match.group(9)),
+            'bytes': bytes_sent,
+            'referer': match.group(11),
+            'user_agent': match.group(12)
+        }
+    except Exception as e:
+        print(f"Error al parsear línea: {e}")
+        return None
+
+def campo_texto(texto=None, limite=None):
+    if texto:
+        texto = texto.strip()
+        if limite:
+            texto = texto[:limite]
+        return texto
+    return None
+
+def insertar_log(pg_conn, logs_data):
+    try:
+        with pg_conn.cursor() as cursor:
+            sql = '''
+            INSERT INTO acceso (
+                id, host, ip, remote_user, auth_user, timestamp, 
+                method, path, protocol, status, bytes, 
+                referer, user_agent
+            ) VALUES (nextval('acceso_id_seq'), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            '''
+                        
+            values = [(
+                campo_texto(log['host'], 100),
+                campo_texto(log['ip'], 50),
+                campo_texto(log['remote_user'], 100),
+                campo_texto(log['auth_user'], 250),
+                campo_texto(log['timestamp'], 50),
+                campo_texto(log['method'], 30),
+                campo_texto(log['path'], 255),
+                campo_texto(log['protocol'], 50),
+                log['status'],
+                log['bytes'],
+                campo_texto(log['referer'], 255),
+                campo_texto(log['user_agent'], 100)
+            ) for log in logs_data]
+                    
+            cursor.executemany(sql, values)
+            pg_conn.commit()
+            print(f"Insertados {len(logs_data)} registros")
+
+    except Exception as e:
+        print(f"Error al insertar registro: {e}")
+        raise
+
+def procesar_archivo(pg_conn, archivo_log='/home/desarrollo/Escritorio/access.log'):
+    try:
+        #crear_tabla(pg_conn) 
+        batch_size = 1000
+        logs_data = []      
+        with open(archivo_log, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    log_data = parse_log_line(line)
+                    if log_data:
+                        logs_data.append(log_data)
+
+                        if len(logs_data) >= batch_size:
+                            insertar_log(pg_conn, logs_data)
+                            logs_data = []
+            if logs_data:
+                insertar_log(pg_conn, logs_data)            
+        print("Procesamiento del archivo completado con éxito.")
+    except FileNotFoundError:
+        print(f"Error: El archivo {archivo_log} no existe.")
+    except Exception as e:
+        print(f"Error durante el procesamiento del archivo: {e}")
+        raise
+    
+if __name__ == "__main__":
+    try:
+        pg_conn, pg_cursor = crear_conexiones()        
+        procesar_archivo(pg_conn)
+    except Exception as e:
+        print(f"Error durante el procesamiento: {e}")
+        sys.exit(1)
+        
+    finally:
+        cerrar_conexiones(pg_conn)
+
+    
+                
